9a825fc70570ba14d5547a1caed31d69d6281fac
Mauro478SS <maurosebastiansanchez@hotmail.com>
2024-10-16 15:16:37 -0300

    refactor

--- MyPipelines/etl_commodities.py
+++ MyPipelines/etl_commodities.py
@@ -4,48 +4,54 @@ import psycopg2
 import redshift_connector
 from sqlalchemy.engine import create_engine
 
-# URL de la API
-url = 'https://api.fas.usda.gov/api/esr/commodities'
 
-#  API key
-api_key = 'A7e4QEAsGHJcySTf4gumahDPpCStKel6lsKhEG6v'
+def main():
 
-# Encabezados de la solicitud
-headers = {
-    'X-Api-Key': api_key
-}
+    # URL de la API
+    url = 'https://api.fas.usda.gov/api/esr/commodities'
 
-# Hacer la solicitud GET a la API con los encabezados
-response = requests.get(url, headers=headers)
+    #  API key
+    api_key = 'A7e4QEAsGHJcySTf4gumahDPpCStKel6lsKhEG6v'
+
+    # Encabezados de la solicitud
+    headers = {
+        'X-Api-Key': api_key
+    }
+
+    # Hacer la solicitud GET a la API con los encabezados
+    response = requests.get(url, headers=headers)
 
 
 
-# Verificar que la solicitud fue exitosa
-if response.status_code == 200:
-    # Convertir la respuesta JSON a un DataFrame de pandas
-    data = response.json()
-    df = pd.DataFrame(data)
-    print(df.head())
-    
-       # Conexión a Redshift
-    redshift_endpoint = 'redshift-pda-cluster.cnuimntownzt.us-east-2.redshift.amazonaws.com'
-    redshift_db = 'pda'
-    redshift_user = '2024_mauro_sebastian_sanchez'
-    redshift_password = 'L4!&9^2$xQ'
-    redshift_port = '5439'
-    redshift_table = 'stg_commodities'
-    redshift_schema = '2024_mauro_sebastian_sanchez_schema'
+    # Verificar que la solicitud fue exitosa
+    if response.status_code == 200:
+        # Convertir la respuesta JSON a un DataFrame de pandas
+        data = response.json()
+        df = pd.DataFrame(data)
+        print(df.head())
+        
+        # Conexión a Redshift
+        redshift_endpoint = 'redshift-pda-cluster.cnuimntownzt.us-east-2.redshift.amazonaws.com'
+        redshift_db = 'pda'
+        redshift_user = '2024_mauro_sebastian_sanchez'
+        redshift_password = 'L4!&9^2$xQ'
+        redshift_port = '5439'
+        redshift_table = 'stg_commodities'
+        redshift_schema = '2024_mauro_sebastian_sanchez_schema'
 
-    # Crear la cadena de conexión
-    conn_str = f'redshift+redshift_connector://{redshift_user}:{redshift_password}@{redshift_endpoint}:{redshift_port}/{redshift_db}'
+        # Crear la cadena de conexión
+        conn_str = f'redshift+redshift_connector://{redshift_user}:{redshift_password}@{redshift_endpoint}:{redshift_port}/{redshift_db}'
 
-    # Crear el motor de SQLAlchemypip 
-engine = create_engine(conn_str)
+        # Crear el motor de SQLAlchemypip 
+    engine = create_engine(conn_str)
 
-try:
-    # Cargar el DataFrame en la tabla de Redshift
-    #with engine.connect() as connection:
-    df.to_sql(name=redshift_table, con=engine, schema=redshift_schema,if_exists='replace', index=False)
-    print(f'Datos cargados en la tabla {redshift_table} de Redshift.')
-except Exception as e:
-    print(f'Error al cargar los datos en Redshift: {e}')
+    try:
+        # Cargar el DataFrame en la tabla de Redshift
+        #with engine.connect() as connection:
+        df.to_sql(name=redshift_table, con=engine, schema=redshift_schema,if_exists='replace', index=False)
+        print(f'Datos cargados en la tabla {redshift_table} de Redshift.')
+    except Exception as e:
+        print(f'Error al cargar los datos en Redshift: {e}')    
+        
+if __name__ == '__main__':
+    main()
--- Tests/test_etl_commodities.py
+++ Tests/test_etl_commodities.py
@@ -1,10 +1,14 @@
 import unittest
 from unittest.mock import patch, MagicMock
-import mi_script
+import sys
+import os
+sys.path.append(os.path.abspath('./MyPipelines'))
+import etl_commodities 
+
 
 class TestMiScript(unittest.TestCase):
 
-    @patch('mi_script.requests.get')
+    @patch('etl_commodities.requests.get')
     def test_api_request(self, mock_get):
         # Configurar el mock para la respuesta de la API
         mock_response = MagicMock()
@@ -13,19 +17,19 @@ class TestMiScript(unittest.TestCase):
         mock_get.return_value = mock_response
 
         # Llamar a la función principal del script
-        mi_script.main()
+        etl_commodities.main()
 
         # Verificar que la solicitud GET fue llamada con la URL correcta
         mock_get.assert_called_once_with('https://api.fas.usda.gov/api/esr/commodities', headers={'X-Api-Key': 'A7e4QEAsGHJcySTf4gumahDPpCStKel6lsKhEG6v'})
 
-    @patch('mi_script.create_engine')
+    @patch('etl_commodities.create_engine')
     def test_redshift_connection(self, mock_create_engine):
         # Configurar el mock para el motor de SQLAlchemy
         mock_engine = MagicMock()
         mock_create_engine.return_value = mock_engine
 
         # Llamar a la función principal del script
-        mi_script.main()
+        etl_commodities.main()
 
         # Verificar que la cadena de conexión fue creada correctamente
         mock_create_engine.assert_called_once_with('redshift+redshift_connector://2024_mauro_sebastian_sanchez:L4!&9^2$xQ@redshift-pda-cluster.cnuimntownzt.us-east-2.redshift.amazonaws.com:5439/pda')
