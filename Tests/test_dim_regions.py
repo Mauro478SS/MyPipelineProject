a5546053836b9aed81881b6d65ec32c8a8ad9362
Mauro478SS <maurosebastiansanchez@hotmail.com>
2024-10-18 00:25:29 -0300

    nuevo test dim_region

--- Tests/test_dim_regions.py
+++ Tests/test_dim_regions.py
@@ -1,12 +1,11 @@
 import unittest
 from unittest.mock import patch, MagicMock
-import sys
 import os
+import sys
+
 
 sys.path.append(os.path.abspath('./MyPipelines'))
 import dim_regions
-
-
 class TestDimRegions(unittest.TestCase):
 
     @patch('dim_regions.create_engine')
@@ -29,12 +28,24 @@ class TestDimRegions(unittest.TestCase):
         mock_connection = mock_engine.connect.return_value.__enter__.return_value
         mock_connection.execute.return_value.rowcount = 5
 
+        # Capturar las consultas ejecutadas
+        executed_queries = []
+
+        def capture_execute(query):
+            executed_queries.append(query)
+            return mock_connection.execute.return_value
+
+        mock_connection.execute.side_effect = capture_execute
+
         # Ejecutar el script principal
         dim_regions.main()
 
-        # Verificar que las consultas SQL fueron ejecutadas correctamente 
-        mock_connection.execute.assert_any_call(
-            """
+        # Imprimir las consultas ejecutadas
+        for query in executed_queries:
+            print(query)
+
+        # Verificar que las consultas SQL fueron ejecutadas correctamente
+        mock_connection.execute.assert_any_call("""
                 UPDATE "test-schema".dim_regions
                 SET end_date = CURRENT_TIMESTAMP,
                     current_record = FALSE
@@ -42,11 +53,9 @@ class TestDimRegions(unittest.TestCase):
                 WHERE dim_regions.regionid = stg_regions.regionid
                 AND dim_regions.regionname <> stg_regions.regionname
                 AND dim_regions.current_record = TRUE;
-            """
-        )
+            """)
         
-        mock_connection.execute.assert_any_call(
-            """
+        mock_connection.execute.assert_any_call("""
                 INSERT INTO "test-schema".dim_regions (regionid, regionname, start_date, end_date, current_record)
                 SELECT regionid::integer, regionname, CURRENT_TIMESTAMP, NULL, TRUE
                 FROM "test-schema".stg_regions
@@ -57,8 +66,7 @@ class TestDimRegions(unittest.TestCase):
                     AND dim_regions.regionname = stg_regions.regionname
                     AND dim_regions.current_record = TRUE
                 );
-            """
-        )
+            """)
 
 if __name__ == '__main__':
     unittest.main()
